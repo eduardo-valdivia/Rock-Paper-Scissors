42e4dff6a69345e3ae742bc59124b2f7c1b676c7
eduardo-valdivia <evaldivia99@gmail.com>
2022-03-01 13:56:26 -0800

    Removing unneccessary comments

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-let playerScore = 0; //initializing the variable to keep player score
-let computerScore = 0; // initializing the variable to keep computer score
-startGame(); //Calling the startGame function to begin the selection process
+let playerScore = 0; //initializing variables to keep scores
+let computerScore = 0; 
+startGame(); //Calling fucntion to start game
 
 function startGame() {
     let playerSelection = window.prompt ("Enter your selection: "); //Asking user for selection
     playerSelection = playerSelection.toLowerCase(); //converting selection to all lower case
-    let computerSelection = computerPlay(); //initializing computer selection and also calling the complay play function
+    let computerSelection = computerPlay(); //initializing computer selection and also calling the computerPlay function
     let msg = playRound(computerSelection, playerSelection); //initializing msg and also calling play round function and passing two arguments
-    console.log(msg); //After playround is ran, displaying what happened in the round
+    console.log(msg); //Display scores
     if (playerScore <= 4 && computerScore <= 4) { //if player score and computer score are less then 5, keep playing!
         console.log("Current Score : Player - " + playerScore + ", Computer - " + computerScore); //display current score
-        setTimeout(startGame, 0); //calling startGame to start next round
+        setTimeout(startGame, 0);
     } else {
         winCondition(); //call win condition function to annouce winner
     }
