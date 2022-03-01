f8e3f7163354a4be93f4c8cd4b791b9e000d2b1a
eduardo-valdivia <evaldivia99@gmail.com>
2022-03-01 11:48:03 -0800

    Added score keeping, final score annoucement, winner annoucement

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+let playerScore = 0;
+let computerScore = 0;
+startGame();
+
+function startGame() {
+    let playerSelection = window.prompt ("Enter your selection: ");
+    playerSelection = playerSelection.toLowerCase();
+    let computerSelection = computerPlay();
+    let msg = playRound(computerSelection, playerSelection);
+    console.log(msg);
+    if (playerScore <= 4 && computerScore <= 4) {
+        console.log("Current Score : Player - " + playerScore + ", Computer - " + computerScore);
+        setTimeout(startGame, 0);
+    } else {
+        winCondition();
+    }
+}
+
 function computerPlay () {
     const random = Math.floor(Math.random() * 3);
     if (random === 0) {
@@ -8,36 +26,45 @@ function computerPlay () {
         return 'scissors';
     }
 }
+
 function playRound (computer, player) {
     let comWin = "You lose " + computer + " beats " + player;
     let playWin = "You win " + player + " beats " + computer;
     let tie = "You tied";
     if (computer === 'paper' && player === 'rock') {
+        computerScore++;
         return comWin;
     } else if (computer === 'rock' && player === 'rock') {
         return tie;
     } else if (computer === 'scissors' && player === 'rock') {
+        playerScore++;
         return playWin;
     } else if (computer === 'paper' && player === 'paper') {
+        console.log("Tied scores stayed the same");
         return tie;
     } else if (computer === 'rock' && player === 'paper') {
+        playerScore++;
         return playWin;
     } else if (computer === 'scissors' && player === 'paper') {
+        computerScore++;
         return comWin;
     } else if (computer === 'paper' && player === 'scissors') {
+        playerScore++;
         return playWin;
     } else if (computer === 'rock' && player === 'scissors') {
+        computerScore++;
         return comWin;
     } else if (computer === 'scissors' && player === 'scissors') {
         return tie;
     }
-
 }
 
-let computerSelection = computerPlay();
-let playerSelection = window.prompt ("Enter your selection: ");
-playerSelection = playerSelection.toLowerCase();
-playRound(computerSelection, playerSelection);
-
-
-console.log(playRound(computerSelection, playerSelection));
+function winCondition() {
+    console.log("Final Score : Player - " + playerScore + ", Computer - " + computerScore);
+    if (playerScore >= 5){
+        console.log("Player wins!");
+    } 
+    if (computerScore >= 5) {
+        console.log("Computer wins!");
+    }
+}
